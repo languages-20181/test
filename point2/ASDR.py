172a5ec85494864e5cdf715ed0ceaa4b824bf328
jdpp95 <jdporrasp@unal.edu.co>
2018-04-28 15:19:17 -0500

    Pseudocode implementation, syntax error printing pending

--- point2/ASDR.py
+++ point2/ASDR.py
@@ -1,4 +1,49 @@
 #Checks if tokens are valid.
+def A():
+    if token == "big" or token == "bus" or token == "cat":
+        B()
+        C()
+    elif token == "ant":
+        match("ant")
+        A()
+        match("all")
+    else:
+        pass #syntax_error("big", "bus", "cat", "ant")
+
+def B():
+    if token == "big":
+        match ("big")
+        C()
+    elif token == "bus":
+        match ("bus")
+        A()
+        match("boss")
+    else:
+        match("")
+
+def C():
+    if token == "cat":
+        match("cat")
+    elif token == "cow":
+        match("cow")
+    else:
+        pass #syntax_error("cat", "cow")
+
+def next_token():
+    token = tokens.pop(0)
+    if token not in terminals:
+        print("Error lexico: No se reconoce \"" + token + "\"")
+        exit(1)
+
+def match(expected):
+    #global token
+    if token == expected:
+        next_token()
+    else:
+        syntax_error(expected)
+
+def syntax_error():
+    pass
 
 print("Analisis Sintactico Descendente Recursivo\n")
 print("Gramatica: \n")
@@ -10,7 +55,7 @@ print("B -> <empty>")
 print("C -> cat")
 print("C -> cow\n\n")
 print("Escriba los tokens separados por espacios")
-tokens = raw_input().split()
+tokens = raw_input().split() #Input (Modify this for file input)
 terminals = ["ant",
 			 "all",
 			 "big",
@@ -20,13 +65,6 @@ terminals = ["ant",
 			 "cow"
 			 ]
 
-lexer(tokens)
-
-for token in tokens:
-
-	#Checks if tokens are valid
-	if token not in terminals:
-		print("Error lexico: No se reconoce \""+token+"\"")
-		exit(1)
-
-	A() #Initial symbol
+token = ""
+next_token()
+A() #Initial symbol
